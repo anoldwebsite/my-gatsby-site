a2002336e07506fb7361fd1c7b836db4d0c26591
Emil Jan <anoldwebsite@gmail.com>
2021-08-21 09:20:16 +0100

    inserted static image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,22 @@
 import * as React from 'react';
 //import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import {StaticImage} from 'gatsby-plugin-image';
 
 //Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>This blog contains posts about different books that I read.</p>
+      <StaticImage 
+        src="https://i.imgur.com/FpnMQL1.jpg"
+        alt="Image of the founder of Shadsoft"
+      />
+      <p>Here is an image of his first grand child.</p>
+      <StaticImage
+        src="../images/Russell fatty.jpg"
+        alt="Dilshad Rana's grand son"
+      />
     </Layout>
     // <main>
     //   <title>Home Page</title>
