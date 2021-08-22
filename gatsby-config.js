c4e9b050bdf85829f4f6d6bace95f412ee436a6d
Emil Jan <anoldwebsite@gmail.com>
2021-08-22 19:31:36 +0100

    Blog contents shown with image in Blog contents

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         name: `blog`,
         path: `${__dirname}/blog`, // (__dirname is a variable from Node.js that stores the absolute path for the directory that contains the file currently being run.)
       }
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        maxWidth: 800,
+        wrapperStyle: fluidResult => `flex:${_.round(fluidResult.aspectRatio, 2)};`,
+      },
     }
   ],
 };
